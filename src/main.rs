88b122f2920d81f27861fb56469f4c676fe9a8a2
Taisuke Fukuno <fukuno@jig.jp>
2020-07-25 13:38:11 +0900

    sample kawakudari

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,28 @@ use rust4ij::std15::*;
 
 #[link_section = ".main"]
 #[no_mangle]
-fn main(_param:i32, _ram:i32, _rom:i32, _divfunc: fn(u32, u32) -> u64) -> i32 {
-    putstr(b"aaaaaaa\0" as *const u8);
-    return 0;
+fn main(_param: i32, _ram: i32, _rom: i32, _divfunc: fn(u32, u32) -> u64) -> i32 {
+    cls();
+    let mut x: u32 = 15;
+    let mut score: u32 = 0;
+    loop {
+        locate(x, 5);
+        putc(236 as char);
+        locate(rnd(32), 23);
+        putc('*');
+        putc(10 as char);
+        wait(3);
+        let c: u32 = inkey();
+        if c == 28 {
+            x -= 1;
+        }
+        if c == 29 {
+            x += 1;
+        }
+        if scr(x, 5) != 0 {
+            break;
+        }
+        score += 1;
+    }
+    return score as i32;
 }
-
